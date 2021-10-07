01e5557af48a9d5438277d4a6ea7a85cdbd60012
SMHussainEdu <65803147+SMHussainEdu@users.noreply.github.com>
2021-10-07 16:01:10 +0530

    Update ArraySum.java

--- src/ArraySum.java
+++ src/ArraySum.java
@@ -1,9 +1,41 @@
-
-public class ArraySum {
-
-	public static void main(String[] args) {
+mahaboobhussain.s@vishnu.edu.in
+import java.util.*;
+class ArraySum
+{
+	
+	public static void main(String[] args) 
+	{
+	
+	int size;
 		
-
+	int []arr;
+		
+	Scanner sc = new Scanner(System.in);
+		
+	size = sc.nextInt(); // read size
+		
+	arr = new int[size]; // declare arr size
+		
+		for (int i=0;i<size ;i++ ) { // read values into array
+			
+		arr[i] = sc.nextInt();
+		
+	}
+		
+	
+	int sum = 0;
+		
+		for (int i=0; i<size ;i++ ) 
+		{
+			
+		sum += arr[i]; // sum = sum + arr[i]
+		
+		}
+		
+	System.out.print(sum);      // sum of array
+		
+	System.out.print(sum/size); // avg of array
+	
 	}
 
 }
